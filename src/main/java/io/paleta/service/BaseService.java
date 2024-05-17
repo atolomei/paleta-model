142187f4e83f44ce9a0cd4c82e54c20f0804c3dd
Alejandro Tolomei <atolomei@novamens.com>
2024-05-17 10:20:47 -0300

    pom

---
+++ src/main/java/io/paleta/service/BaseService.java
@@ -0,0 +1,21 @@
+package io.paleta.service;
+
+
+import io.paleta.logging.Logger;
+import io.paleta.model.JsonObject;
+
+public class BaseService extends JsonObject {
+
+		
+		static private Logger logger = Logger.getLogger(BaseService.class.getName());
+				
+		
+		public BaseService() {
+		}
+		
+
+		
+		
+		
+
+}
